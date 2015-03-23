115985c44649fcee879fa120537c27c517835cc6
Bin Zhang <sjtuzb@gmail.com>
2015-03-23 16:34:27 +0800

    add Launch OpenStack Instance

---
+++ launch_openstack_instance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Launch OpenStack Instance
+#
+# author: Bin Zhang
+# email: sjtuzb@gmail.com
+#
+cd ~
+apt-get update
+
+# Install git
+apt-get install -y git
+# Install vim
+apt-get install -y vim
+
