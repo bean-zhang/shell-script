5d206e95db9c34ca0e7651f68d9915ce0a17db47
Bin Zhang <sjtuzb@gmail.com>
2015-04-16 12:37:03 +0800

    add macos

---
+++ macos/wireshark_capture_privileges.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# wirshark capture privileges
+#
+# author: Bin Zhang
+# sjtuzb@gmail.com
+#
+chown ZB /dev/bpf*
+
--- upgrade_all_packages_with_pip.sh
+++ upgrade_all_packages_with_pip.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#
+# upgrade all packages with pip
+#
+# author: Bin Zhang
+# email: sjtuzb@gmail.com
+@
+
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
