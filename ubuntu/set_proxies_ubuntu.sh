7d3df647d895d9c2db54ac6cf12e08c3b7ba11bc
Bin Zhang <sjtuzb@gmail.com>
2015-04-23 17:11:09 +0800

    modify set_proxy

--- ubuntu/set_proxies_ubuntu.sh
+++ ubuntu/set_proxies_ubuntu.sh
@@ -9,7 +9,6 @@ cp ~/git/proxy.sh /etc/profile.d/
 cp ~/git/95proxies /etc/apt/apt.conf.d/
 
 source /etc/profile.d/proxy.sh
-source /etc/apt/apt.conf.d/95proxies
 
 # Verify that apt has picked up these settings via this command
 apt-config dump  | grep -i proxy # lists the proxy settings
