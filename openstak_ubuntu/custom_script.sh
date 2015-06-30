c734aa034252c0adc82fcd1c881c886e1a64d1f1
Bin Zhang <sjtuzb@gmail.com>
2015-06-30 13:45:50 +0800

    add custom_script.sh, modify proxy.sh

---
+++ openstak_ubuntu/custom_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# custom script when launch an instance in openstack
+#
+# author: Bin Zhang
+# email: sjtuzb@gmail.com
+#
+
+passwd ubuntu<<EOF
+#password
+your_password
+your_password
+EOF
+
+apt-get update
+apt-get install -y git
+
+mkdir git
+cd git
+git clone https://github.com/87boy/dot-files.git
+git clone git@github.com:87boy/shell-script.git
+`
--- ubuntu/proxy.sh
+++ ubuntu/proxy.sh
@@ -7,12 +7,12 @@
 #
 
 export http_proxy=http://proxy.example.com:8080/
-export https_proxy=$http_proxy
-export ftp_proxy=$http_proxy
+export https_proxy=https://proxy.example.com:8080/
+export ftp_proxy=ftp://proxy.example.com:8080/
 export rsync_proxy=$http_proxy
 export no_proxy="127.0.0.1,localhost,local_address,local_domain.com"
 export HTTP_PROXY=$http_proxy
-export HTTPS_PROXY=$http_proxy
-export FTP_PROXY=$http_proxy
-export RSYNC_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+export FTP_PROXY=$ftp_proxy
+export RSYNC_PROXY=$rsync_proxy
 export NO_PROXY=$no_proxy
