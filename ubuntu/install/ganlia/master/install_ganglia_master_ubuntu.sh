1455721d290c834d619af22ea25395d5bf0a09b5
Bin Zhang <sjtuzb@gmail.com>
2015-10-04 12:36:05 +0000

    install ganglia master ubuntu

--- ubuntu/install/ganlia/master/install_ganglia_master_ubuntu.sh
+++ ubuntu/install/ganlia/master/install_ganglia_master_ubuntu.sh
@@ -6,10 +6,24 @@
 # email: sjtuzb@gmail.com
 #
 
-cd ~
 # Install Ganglia Monitor, RRDtool, Gmetad and the Ganglia web front end
-apt-get install -y ganglia-monitor rrdtool gmetad ganglia-webfrontend
+sudo apt-get install -y ganglia-monitor rrdtool gmetad ganglia-webfrontend
 # Set up the online graphical dashboard by copying the Ganglia web front end
 # configuration file to the Apache sites-enabled folder
-cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf
-# 
+sudo cp /etc/ganglia-webfrontend/apache.conf /etc/apache2/sites-enabled/ganglia.conf
+# config gmetad and gmond
+gmetad_conf=./unicast.gmetad.conf
+gmond_conf=./unicast.gmond.conf
+if [ -f "${gmetad_conf}" ]
+then
+	sudo mv /etc/ganglia/gmetad.conf /etc/ganglia/gmetad.conf.bak
+	sudo cp ${gmetad_conf} /etc/ganglia/gmetad.conf
+fi
+if [ -f "${gmond_conf}" ]
+then
+	sudo mv /etc/ganglia/gmond.conf /etc/ganglia/gmond.conf.bak
+	sudo cp ${gmond_conf} /etc/ganglia/gmond.conf
+fi
+# restart all the services related to ganglia
+#sudo service ganglia-monitor restart && sudo service gmetad restart && sudo service apache2 restart
+
