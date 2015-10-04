beeac3410626d8ba6a2c2363fa5e7529b193f4a9
Bin Zhang <sjtuzb@gmail.com>
2015-10-04 19:25:12 +0800

    unset_proxy.sh

--- centos/proxy/set_proxies_centos.sh
+++ centos/proxy/set_proxies_centos.sh
@@ -5,16 +5,32 @@
 # author: Bin Zhang
 # email: sjtuzb@gmail.com
 #
-cp ~/git/shell-script/centos/proxy/proxy.sh /etc/profile.d/
-#cp ~/git/95proxies /etc/apt/apt.conf.d/
+sudo cp ~/git/shell-script/centos/proxy/proxy.sh /etc/profile.d/
+#sudo cp ~/git/95proxies /etc/apt/apt.conf.d/
 
 source /etc/profile.d/proxy.sh
 
 # Verify that apt has picked up these settings via this command
 #apt-config dump  | grep -i proxy # lists the proxy settings
+env | grep proxy
 
 # Backup /etc/yum.conf
-cp /etc/yum.conf /etc/yum.conf.bak
+sudo cp /etc/yum.conf /etc/yum.conf.bak
 # Configure yum
-echo "proxy=http://proxy.example.com:port/" >> /etc/yum.conf
+sudo echo "proxy=http://proxy.example.com:port/" >> /etc/yum.conf
+
+ssh_dir=~/.ssh
+if [ ! -d "${ssh_dir}" ]
+then
+	mkdir -p ~/.ssh
+fi
+config_file=${ssh_dir}/config
+if [ ! -f "${config_file}" ]
+then
+	touch ${config_file}
+fi
+
+echo "Host				git.example.com" >> ${config_file}
+echo "User				git" >> ${config_file}
+echo "ProxyCommand		/bin/nc -X connect -x proxy_ip:proxy_port %h %p" >> ${config_file}
 
---
+++ ubuntu/proxy/unset_proxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# unset the proxy
+#
+# author: Bin Zhang
+# email: sjtuzb@gmail.com
+#
+
+unset http_proxy https_proxy ftp_proxy rsync_proxy no_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY NO_PROXY
+
